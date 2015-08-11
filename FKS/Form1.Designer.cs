dae46b4e799e2d5aabb9d1c2b8c2e5d6876b3ccb
homebox <homebox25136@gmail.com>
2015-08-11 22:17:12 +0900

    v0.0.1

---
+++ FKS/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace FKS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keyboardHook1 = new HongliangSoft.Utilities.Gui.KeyboardHook();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "FKSがバックグラウンドで起動しています";
+            this.notifyIcon1.BalloonTipTitle = "Function key Settings";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "FKS";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.ExitToolStripMenuItem.Text = "終了";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // keyboardHook1
+            // 
+            this.keyboardHook1.KeyboardHooked += new HongliangSoft.Utilities.Gui.KeyboardHookedEventHandler(this.keyboardHook1_KeyboardHooked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "FunctionKeySetting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private HongliangSoft.Utilities.Gui.KeyboardHook keyboardHook1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ FKS/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace FKS
+{
+    public partial class Form1 : Form
+    {
+
+        int b = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.notifyIcon1.ShowBalloonTip(500);
+        }
+
+        private void keyboardHook1_KeyboardHooked(object sender, HongliangSoft.Utilities.Gui.KeyboardHookedEventArgs e)
+        {
+            //Console.WriteLine(e.KeyCode.ToString("d"));
+
+            var a = int.Parse(e.KeyCode.ToString("d"));
+
+            if (b == 0)
+            {
+
+                //F9キーが押されたか調べる
+                if (a == 120)
+                {
+                    //Console.WriteLine("F9キーが押されました。");
+
+                    var sim = new InputSimulator();
+                    sim.Keyboard.ModifiedKeyStroke(
+                      new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
+                      VirtualKeyCode.LEFT
+                    );
+
+                    b = 1;
+                }
+
+                //F10キーが押されたか調べる
+                if (a == 121)
+                {
+                    //Console.WriteLine("F10キーが押されました。");
+
+                    var sim = new InputSimulator();
+                    sim.Keyboard.ModifiedKeyStroke(
+                      new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
+                      VirtualKeyCode.RIGHT
+                    );
+
+                    b = 1;
+                }
+
+            } else {
+                b = 0;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
