00fc1135974e5d14816f9eba9aacd1cca9baca41
こめ <come25136@gmail.com>
2016-04-13 21:48:34 +0900

    移動キーを自由化

--- FKS/Form1.cs
+++ FKS/Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
@@ -8,7 +11,10 @@ namespace FKS
     public partial class Form1 : Form
     {
 
-        int b = 0;
+        string a;
+        int b;
+        int c;
+        int flag;
 
         public Form1()
         {
@@ -18,53 +24,93 @@ namespace FKS
         private void Form1_Load(object sender, EventArgs e)
         {
             this.notifyIcon1.ShowBalloonTip(500);
+            pictureBox1.LoadAsync("http://www.paper-glasses.com/api/twipi/come25136/bigger");
+            textBox1.Text = Properties.Settings.Default.Leftkey;
+            textBox2.Text = Properties.Settings.Default.Rightkey;
         }
 
         private void keyboardHook1_KeyboardHooked(object sender, HongliangSoft.Utilities.Gui.KeyboardHookedEventArgs e)
         {
-            //Console.WriteLine(e.KeyCode.ToString("d"));
+            Console.WriteLine(e.KeyCode);
 
-            var a = int.Parse(e.KeyCode.ToString("d"));
-
-            if (b == 0)
+            a = e.KeyCode.ToString();
+            if (c != 1)
             {
-
-                //F9キーが押されたか調べる
-                if (a == 120)
+                if (b == 0)
                 {
-                    //Console.WriteLine("F9キーが押されました。");
 
-                    var sim = new InputSimulator();
-                    sim.Keyboard.ModifiedKeyStroke(
-                      new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
-                      VirtualKeyCode.LEFT
-                    );
+                    //左移動
+                    if (a == Properties.Settings.Default.Leftkey)
+                    {
+                        //Console.WriteLine("F9キーが押されました。");
 
-                    b = 1;
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(
+                          new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
+                          VirtualKeyCode.LEFT
+                        );
+
+                        b = 1;
+                    }
+
+                    //右移動
+                    if (a == Properties.Settings.Default.Rightkey)
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(
+                          new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
+                          VirtualKeyCode.RIGHT
+                        );
+
+                        b = 1;
+                    }
+
                 }
-
-                //F10キーが押されたか調べる
-                if (a == 121)
+                else
                 {
-                    //Console.WriteLine("F10キーが押されました。");
-
-                    var sim = new InputSimulator();
-                    sim.Keyboard.ModifiedKeyStroke(
-                      new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN },
-                      VirtualKeyCode.RIGHT
-                    );
-
-                    b = 1;
+                    b = 0;
                 }
-
-            } else {
-                b = 0;
             }
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            flag = 1;
             Application.Exit();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox1.Text = a;
+            Properties.Settings.Default.Leftkey = a;
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox2.Text = a;
+            Properties.Settings.Default.Rightkey = a;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (flag != 1)
+            {
+                e.Cancel = true;
+                c = 0;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            c = 1;
+            this.Visible = true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Visible = false;
         }
     }
 }
